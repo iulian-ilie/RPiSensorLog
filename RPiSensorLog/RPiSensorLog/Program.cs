51c1cdf932d3301d99ccb04f065f33f7814d5331
iulian-ilie <iulian84@gmail.com>
2015-02-11 00:20:18 +0200

    rpi test build

---
+++ RPiSensorLog/RPiSensorLog/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPiSensorLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            helpScreen();
+            Console.ReadKey();
+        }
+
+        static void helpScreen()
+        {
+            Console.Write(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+            Console.WriteLine(" Ver. " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine(String.Format("OS: {0}", Environment.OSVersion));
+            Console.WriteLine(string.Format("Local Time: {0}", DateTime.Now));
+            Console.WriteLine();
+            Console.WriteLine("[-help] Shows this screen");
+            Console.WriteLine("[-view] <delay> View connected sensors with refresh on <delay>");
+            Console.WriteLine("[-start] <delay> <file> Start logging to <file>");
+            Console.WriteLine("[-stop] Stop logging");
+            Console.WriteLine("Default values for <optional parameters> can be edited in the options.cfg file.");
+            Console.WriteLine();
+            //Console.WriteLine("Press any key to exit");
+        }
+    }
+}
